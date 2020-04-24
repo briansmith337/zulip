62cf30de14a7f18d6bef5e88a375097b6493b8c8
briansmith337 <briansmith337@gmail.com>
2020-04-24 12:46:22 +0200

    committing sooner than I would like.  Main reason was the realm_description - changing the default there.

--- zerver/lib/realm_description.py
+++ zerver/lib/realm_description.py
@@ -6,7 +6,7 @@ from zerver.lib.html_to_text import html_to_text
 
 @cache_with_key(realm_rendered_description_cache_key, timeout=3600*24*7)
 def get_realm_rendered_description(realm: Realm) -> str:
-    realm_description_raw = realm.description or "The coolest place in the universe."
+    realm_description_raw = realm.description or "The world's only community protecting your PRIVACY and FREE SPEECH"
     return bugdown_convert(realm_description_raw, message_realm=realm,
                            no_previews=True)
 
