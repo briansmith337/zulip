764bfd9be60c639833908bdd5c14bfbd71f74b11
briansmith337 <briansmith337@gmail.com>
2020-04-24 14:02:27 +0200

    Mainly email message formatting

--- zerver/lib/realm_description.py
+++ zerver/lib/realm_description.py
@@ -6,7 +6,7 @@ from zerver.lib.html_to_text import html_to_text
 
 @cache_with_key(realm_rendered_description_cache_key, timeout=3600*24*7)
 def get_realm_rendered_description(realm: Realm) -> str:
-    realm_description_raw = realm.description or "The world's only community protecting your PRIVACY and FREE SPEECH"
+    realm_description_raw = realm.description or "Defending your rights to PRIVACY and FREE SPEECH"
     return bugdown_convert(realm_description_raw, message_realm=realm,
                            no_previews=True)
 
